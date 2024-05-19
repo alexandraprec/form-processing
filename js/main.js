c64ce0696fd9dc830016eb337eda1c7a50d31b8a
alexandraprec <166450856+alexandraprec@users.noreply.github.com>
2024-05-18 23:08:07 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,3 +16,27 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+
+//Title Case
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+if(queryString.length > 0){//data submitted
+  let myOutput = `<div id="container">`;
+
+  urlParams.forEach(function(value,key){
+
+    // Check if the key is one of the elements that need to be changed to title case
+    if (key === "First_Name" || key === "Last_Name" || key === "Address" || key === "City") {
+      // Change the value to title case
+      value = value.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    }
+
+    key = key.split("_").join(" ");
+    myOutput += `<p>${key}: ${value}</p>`;
+  });
+  myOutput += `<p><a href="index.html">CLEAR</a></p>`; 
+  myOutput += `</div>`;
+  document.querySelector("#output").innerHTML = myOutput;
+}
